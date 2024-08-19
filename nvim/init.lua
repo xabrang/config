7d99e68979f1208c5d56705a49bd3e77fc153015
xabrang <sxabrang@gmail.com>
2024-08-19 13:11:40 +0700

    add tmux navigation to nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -58,10 +58,21 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	{
-		"cranberry-clockworks/coal.nvim",
-		config = function()
-			require("coal").setup()
-		end,
+		"christoomey/vim-tmux-navigator",
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+		},
+		keys = {
+			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
 	},
 	"tpope/vim-sleuth",
 	{
