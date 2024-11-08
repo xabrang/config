29f63df139edc6f52ca15852cab59496d3382083
xabrang <sxabrang@gmail.com>
2024-11-08 22:05:08 +0700

    up

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,4 @@
--- vim: ts=2 sts=2 sw=2 et
-vim.o.background = "light"
+-- vim: ts=2 sts=2 sw=2
 vim.cmd.colorscheme("vim")
 vim.opt.colorcolumn = "80"
 
@@ -69,49 +68,17 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	{
-		"miikanissi/modus-themes.nvim",
+		"Mofiqul/adwaita.nvim",
+		lazy = false,
 		priority = 1000,
+		-- configure and set on startup
 		config = function()
-			require("modus-themes").setup({
-				-- Theme comes in two styles `modus_operandi` and `modus_vivendi`
-				-- `auto` will automatically set style based on background set with vim.o.background
-				style = "auto",
-				variant = "default", -- Theme comes in four variants `default`, `tinted`, `deuteranopia`, and `tritanopia`
-				transparent = false, -- Transparent background (as supported by the terminal)
-				dim_inactive = true, -- "non-current" windows are dimmed
-				hide_inactive_statusline = false, -- Hide statuslines on inactive windows. Works with the standard **StatusLine**, **LuaLine** and **mini.statusline**
-				styles = {
-					-- Style to be applied to different syntax groups
-					-- Value is any valid attr-list value for `:help nvim_set_hl`
-					comments = {},
-					keywords = {},
-					functions = { bold = true },
-					variables = {},
-				},
-
-				--- You can override specific color groups to use other groups or a hex color
-				--- Function will be called with a ColorScheme table
-				--- Refer to `extras/lua/modus_operandi.lua` or `extras/lua/modus_vivendi.lua` for the ColorScheme table
-				---@param colors ColorScheme
-				on_colors = function(colors) end,
-
-				--- You can override specific highlights to use other groups or a hex color
-				--- Function will be called with a Highlights and ColorScheme table
-				--- Refer to `extras/lua/modus_operandi.lua` or `extras/lua/modus_vivendi.lua` for the Highlights and ColorScheme table
-				---@param highlights Highlights
-				---@param colors ColorScheme
-				on_highlights = function(highlights, colors)
-					highlights.CursorLine = { bg = colors.bg_status_line_active }
-					highlights.CursorLineNr = { bg = colors.bg_status_line_active }
-					highlights.LineNr = { bg = colors.bg_status_line_active }
-					highlights.ColorColumn = { bg = colors.bg_status_line_active }
-					highlights.SignColumn = { bg = colors.bg_status_line_active }
-				end,
-			})
-			vim.cmd([[colorscheme modus]])
+			vim.g.adwaita_darker = false
+			vim.g.adwaita_disable_cursorline = false
+			vim.g.adwaita_transparent = true
+			vim.cmd("colorscheme adwaita")
 		end,
 	},
-
 	"tpope/vim-sleuth",
 
 	{
@@ -132,7 +99,7 @@ require("lazy").setup({
 		event = "VimEnter",
 		opts = {
 			icons = {
-				mappings = vim.g.have_nerd_font,
+				mappings = false,
 				keys = vim.g.have_nerd_font and {} or {
 					Up = "<Up> ",
 					Down = "<Down> ",
@@ -554,6 +521,12 @@ require("lazy").setup({
 			vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
 			vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
 		end,
+	},
+	{
+		"vimpostor/vim-lumen",
+		lazy = false,
+		priority = 1000,
+		config = function() end,
 	},
 })
 
